a54293d32907c976c83600d90dc04953b2ee7977
yang <596624224@qq.com>
2022-11-20 21:21:19 +0800

    深拷贝

---
+++ src/components/deepCopy.vue
@@ -0,0 +1,19 @@
+<template>
+    <button @click="deepCopy">深拷贝</button>
+</template>
+
+<script setup lang="ts">
+// 深拷贝
+const deepCopy = () => {
+    
+}
+</script>
+
+<style scoped lang="less">
+.deepcopy_button{
+    button {
+        width: 150px;
+        height: 50px;
+    }
+}
+</style>
