ddacd8cc149d52099fd6f2b36fae458eed36215c
yang <596624224@qq.com>
2022-11-21 17:20:04 +0800

    '深拷贝'

--- src/components/deepCopy.vue
+++ src/components/deepCopy.vue
@@ -1,12 +1,44 @@
 <template>
-    <button @click="deepCopy">深拷贝</button>
+    <button @click="deepCopy1">深拷贝</button>
 </template>
 
 <script setup lang="ts">
-// 深拷贝
-const deepCopy = () => {
-    
+import { reactive, ref } from "@vue/reactivity"
+
+interface personData {
+    name: string,
+    age: number,
+    charact: number
 }
+
+const copyData:personData = reactive({
+    name: 'zy',
+    age: 26,
+    charact: Infinity
+})
+
+// 第一版深拷贝
+    // 问题: 1、时间对象变成字符串   
+    // 2、RegExp、Error对象，会变成空对象{}
+    // 3、function、undefined。会被丢失
+    // 4、number类型中的NaN、Infinity、-Infinity，会变成null
+    // 5、对象中循环引用也无法实现
+const deepCopy1 = () => {
+    console.log('没copy之前', copyData);
+    let copyData1 = JSON.parse(JSON.stringify(copyData))
+    console.log('copy之后', copyData1);
+}
+
+// 之后就逐渐完善的实现一个深copy
+// 
+const deepCopy2 = () => {
+    console.log('没copy之前', copyData);
+    let copyData1 = JSON.parse(JSON.stringify(copyData))
+    console.log('copy之后', copyData1);
+}
+
+
+
 </script>
 
 <style scoped lang="less">
